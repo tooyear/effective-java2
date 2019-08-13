2359f80fcc47598b17cc1b89729afe8a6fd2bccd
tooyear <2316421531@qq.com>
2019-08-13 23:39:56 +0800

    匿名类、局部类笔记

--- src/com/tooyear/fourth/Article22.java
+++ src/com/tooyear/fourth/Article22.java
@@ -11,16 +11,48 @@ public class Article22 {
         System.out.println("初始化外部类");
     }
 
+    /**
+     * 非静态成员类
+     */
     public class Article22InnerClass {
         public Article22InnerClass() {
             System.out.println("初始化非静态内部类");
         }
     }
 
+    /**
+     * 静态成员类
+     */
     public static class Article22InnerStaticClass {
         public Article22InnerStaticClass() {
             System.out.println("初始化静态内部类");
         }
     }
 
+    /**
+     * 成员匿名类
+     */
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("成员匿名类");
+        }
+    };
+    /**
+     * 匿名内部类
+     */
+    public void noNameInnerClass(){
+        Thread innerRunnable = new Thread();
+
+    }
+
+    /**
+     * 局部类
+     */
+    public void partialClassDemo() {
+        class partial {
+
+        }
+        new partial();
+    }
 }
